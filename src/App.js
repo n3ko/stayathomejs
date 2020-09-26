7328d0b55f2c82362e88f2b4f7ae76882d21918a
n3ko <n3ko.74@gmail.com>
2020-09-26 17:14:41 +0200

    header

--- src/App.js
+++ src/App.js
@@ -140,7 +140,6 @@ function App () {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<h1>StayAtHome exercises</h1>
 				<nav>
 					<ul>
 						<li onClick={ () => setActiveTab(1) }>Luxor</li>
